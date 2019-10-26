c9eff0071c7cfaa900f91ad52b75fa5d5104a170
FrancaTM <francatm@outlook.com>
2019-10-26 04:28:56 -0300

    implement location class

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:flutter_clima_app/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,14 +7,10 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  Future<void> getLocation() async {
-    try {
-      Position position = await Geolocator()
-          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-      print(position);
-    } catch (e) {
-      print(e);
-    }
+  void getLocation() async {
+    final location = Location();
+    await location.getCurrentLocation();
+    print('Current location: ${location.latitude}, ${location.longitude}');
   }
 
   @override
